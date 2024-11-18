1fbdaeccb06eff2709da4b1cac360fe45151a5dc
RCatMin <razzcat_min@naver.com>
2024-11-18 11:48:56 +0900

    init commit

--- src/textrpg/Main.java
+++ src/textrpg/Main.java
@@ -3,7 +3,8 @@ package textrpg;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TextRPG game = TextRPG.getInstance();
+		game.run();
 
 	}
 
